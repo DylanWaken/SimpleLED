00c858f5781facd61904e792586ac50ee2411d18
dylan <dylansun@usc.edu>
2022-12-23 17:21:20 -0800

    firstTest

--- main.cpp
+++ main.cpp
@@ -3,5 +3,14 @@
 #include "driver.h"
 
 int main() {
+    uint8_t* arr = (uint8_t*)malloc(4 * 16 * 16 * sizeof(uint8_t));
+    for(int i = 0; i < 16 * 16; i++){
+        arr[i] = i;
+        arr[i + 16 * 16] = i;
+        arr[i + 2 * 16 * 16] = i;
+        arr[i + 3 * 16 * 16] = i;
+    }
 
+    init();
+    matrixRender(arr);
 }
