b5ce0a561e9fb1a3fcd6a7ce64b7b3321eec69a6
dylan <dylansun@usc.edu>
2022-12-23 18:38:19 -0800

    exp2

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main() {
     uint8_t* arr = malloc(4 * 16 * 16 * sizeof(uint8_t));
-    for(int prime = 1; prime < 255; prime++) {
+    for(int prime = 1; prime <16*5; prime++) {
         for (int i = 0; i < prime; i++) {
             arr[i] = i;
             arr[i + 16 * 16] = 255;
@@ -13,7 +13,7 @@ int main() {
         init();
 
         renderMat(arr);
-        usleep(500000);
+        usleep(200000);
         fprintf(stdout, "prime: %d", prime);
     }
 
